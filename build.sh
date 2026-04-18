aecdb4ece599345d12964b7836508da36051a525
ganesh5356 <khamitkarganeshkumar@gmail.com>
2026-04-18 21:00:44 +0530

    feat: auto-create superuser during build via env vars

--- build.sh
+++ build.sh
@@ -8,3 +8,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Auto-create superuser from environment variables (only if DJANGO_SUPERUSER_USERNAME is set)
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser --no-input || true
+fi
